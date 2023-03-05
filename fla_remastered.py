d7931659d12343d1675e69da4495672f7df8e888
pingfanH <89574481+unityful@users.noreply.github.com>
2023-03-05 17:39:43 +0800

    Add files via upload

--- fla_remastered.py
+++ fla_remastered.py
@@ -1,9 +1,14 @@
 import zipfile
 import os
+import sys
 from loading import progress_bar
 from findmovie import linemovie
 print("whats your fla name u want to remastered?(just name not .fla):")
 file=input()
+filename=file
+if(not os.path.isfile(file+".fla")):
+    print("\033[1;31m not a file called : \033[1;31m"+file+".fla")
+    sys.exit(0)
 print("exports name:")
 exportname=input()
 filea=(file+'.fla')
@@ -12,9 +17,8 @@ if(not os.path.isdir(target_path)):
     z = zipfile.ZipFile(filea, 'r')
     z.extractall(path=target_path)
     z.close()
-exportname=(file+'\LIBRARY\exports\\'+exportname+'.xml')
+exportname=(file+'/LIBRARY/exports/'+exportname+'.xml')
 print(exportname)
-shapesname=(file+'\LIBRARY\shapes\.xml')
 file = open(str(exportname),'rb')
 for line in file:
             line=str(line)
@@ -26,12 +30,11 @@ for line in file:
                 movie=movie.replace("\\n","")
                 movie=movie.replace("'","")
                 print(movie)
-                file.close()
-                shapesname=(file+r'\LIBRARY\shapes\\'+movie+'.xml')
+                file=str(file)
+                shapesname=(filename+'/LIBRARY/movieclips/'+movie+'.xml')
                 shapesname=str(shapesname)
                 print(shapesname)
                 file1 = open(str(shapesname),'rb')
-                print(file1)
                 for line in file1:
                         line=str(line)
                         if "shapes/"in line:
@@ -42,3 +45,19 @@ for line in file:
                             movie=movie.replace("\\n","")
                             movie=movie.replace("'","")
                             print(movie)
+                            file2 = str(file)
+                            shapesname = (filename + '/LIBRARY/shapes/' + movie + '.xml')
+                            shapesname = str(shapesname)
+                            file1 = open(str(shapesname), 'rb')
+                            print(shapesname)
+                            for line in file1:
+                                line = str(line)
+                                if "resources/" in line:
+                                    movie = line.split('"resources/')[1]
+                                    movie = movie.replace('"', '')
+                                    movie = movie.replace('>', '')
+                                    movie = movie.replace('/', '')
+                                    movie = movie.replace("\\n", "")
+                                    movie = movie.replace("'", "")
+                                    movie=(movie+".png")
+                                    print(movie)
