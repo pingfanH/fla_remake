f469803b8489729bffac52b7ac0135e438962faa
pingfanH <89574481+unityful@users.noreply.github.com>
2023-03-05 12:58:47 +0800

    Add files via upload

--- fla_remastered.py
+++ fla_remastered.py
@@ -1,10 +1,44 @@
 import zipfile
 import os
-print("whats your fla name u want to convert?(just name not .fla)\n:")
+from loading import progress_bar
+from findmovie import linemovie
+print("whats your fla name u want to remastered?(just name not .fla):")
 file=input()
+print("exports name:")
+exportname=input()
 filea=(file+'.fla')
 target_path=str(file)
 if(not os.path.isdir(target_path)):
     z = zipfile.ZipFile(filea, 'r')
     z.extractall(path=target_path)
     z.close()
+exportname=(file+'\LIBRARY\exports\\'+exportname+'.xml')
+print(exportname)
+shapesname=(file+'\LIBRARY\shapes\.xml')
+file = open(str(exportname),'rb')
+for line in file:
+            line=str(line)
+            if "movieclips/" in line:
+                movie=line.split('"movieclips/')[1]
+                movie=movie.replace('"','')
+                movie=movie.replace('>','')
+                movie=movie.replace('/','')
+                movie=movie.replace("\\n","")
+                movie=movie.replace("'","")
+                print(movie)
+                file.close()
+                shapesname=(file+r'\LIBRARY\shapes\\'+movie+'.xml')
+                shapesname=str(shapesname)
+                print(shapesname)
+                file1 = open(str(shapesname),'rb')
+                print(file1)
+                for line in file1:
+                        line=str(line)
+                        if "shapes/"in line:
+                            movie=line.split('"shapes/')[1]
+                            movie=movie.replace('"','')
+                            movie=movie.replace('>','')
+                            movie=movie.replace('/','')
+                            movie=movie.replace("\\n","")
+                            movie=movie.replace("'","")
+                            print(movie)
